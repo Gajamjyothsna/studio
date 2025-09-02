4d8d939b16227299889f775e2004363aac7a8596
Gajam Jyothsna <jyothsnagajam610@gmail.com>
2025-09-02 09:27:40 +0000

    each roadmap find revelant pictures and add into it

--- src/lib/roadmaps.ts
+++ src/lib/roadmaps.ts
@@ -8,11 +8,11 @@ export const roadmaps = [
     documents: [
       {
         title: "WEB DEV SHEET",
-        link: "https://google.com",
+        link: "https://drive.google.com/file/d/1Az6K7n0_Mdq-D_51wcWjap919qmDKyvB/view?usp=drive_link",
       },
       {
         title: "📅 12-Week Study + Project Plan (MERN Full Stack UI Engineer)",
-        link: "https://google.com",
+        link: "https://drive.google.com/file/d/11LIiAmH5SS2vMY395mYfDShA8yHoYJ1C/view?usp=drive_link",
       },
     ],
   },
@@ -21,11 +21,45 @@ export const roadmaps = [
     slug: "java-developer",
     description: "Your path to becoming a proficient Java developer.",
     image: "https://picsum.photos/600/400",
-    imageHint: "code java",
+    imageHint: "java code",
     documents: [
-      { title: "Core Java Concepts", link: "https://google.com" },
-      { title: "Spring Framework", link: "https://google.com" },
-      { title: "Microservices with Java", link: "https://google.com" },
+      { title: "Java Roadmap Important Videos", link: "https://drive.google.com/file/d/114ZeQsISYB0Aog4YxdJ_VWp-MlETts-K/view?usp=drive_link" },
+      { title: "Java Cheat Sheet", link: "https://drive.google.com/file/d/13ObWkyBM0FuXw6jAh8bP8AtR0C8WSYAo/view?usp=drive_link" },
+    ],
+  },
+  {
+    title: "Data Structures and Algorithms",
+    slug: "data-structures-and-alogrithms",
+    description: "Your path to becoming a proficient in DSA.",
+    image: "https://picsum.photos/600/400",
+    imageHint: "algorithm data",
+    documents: [
+      { title: "Data Structures and Algorithms Book by Narasima", link: "https://drive.google.com/file/d/1YEHwxyC7zbVZ-vrpgCPJ2gq5ebZouLZ7/view?usp=drive_link" },
+      { title: "DSA Cheat Sheet", link: "https://drive.google.com/file/d/1B26btSnHBM8WfQOA1uFMYPemorgzYBl-/view?usp=drive_link" },
+    ],
+  },
+
+  {
+    title: "Placement Series",
+    slug: "placement-series",
+    description: "Your path to becoming a proficient in DSA.",
+    image: "https://picsum.photos/600/400",
+    imageHint: "job interview",
+    documents: [
+      { title: "Best Websites To Build/Upgrade Your Resumes", link: "https://docs.google.com/document/d/1-vkb1N0YVI9EvyE9l2iQ1DAf2U4be2_-/edit?usp=drive_link&ouid=104509574180702970720&rtpof=true&sd=true" },
+      { title: "Projects Sheet", link: "https://drive.google.com/file/d/1ElCTWoJJsALmRPHyf9c4wu_NZWVNicM6/view?usp=drive_link" },
+    ],
+  },
+
+  {
+    title: "AI-ML Developer Roadmap",
+    slug: "ai-ml-developer",
+    description: "Your path to becoming a proficient in DSA.",
+    image: "https://picsum.photos/600/400",
+    imageHint: "machine learning",
+    documents: [
+      { title: "AI-ML Developer sheet", link: "https://docs.google.com/document/d/1c76xmWSUbbRdHzboJMh8-18nH-6T8C08/edit?usp=drive_link&ouid=104509574180702970720&rtpof=true&sd=true" },
+      
     ],
   },
 ];
