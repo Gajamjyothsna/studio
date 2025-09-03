8273745c27d390b401e23bd1dbde81fa8fe635bb
Gajam Jyothsna <jyothsnagajam610@gmail.com>
2025-09-03 04:39:29 +0000

    updates

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',  
+  basePath: '/studio',  
+  assetPrefix: '/studio/', 
   typescript: {
     ignoreBuildErrors: true,
   },
